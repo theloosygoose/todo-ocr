4e8cc83997354d89d6dea1175692ba74f63aa508
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2025-01-09 18:24:26 -0500

    making the model

--- main.py
+++ main.py
@@ -44,6 +44,25 @@ print(f"Using {device} device")
 
 
 # Define Model
+class NueralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512,512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+        def forward(self, x):
+            x = self.flatten(x)
+            logits = self.linear_relu_stack(x)
+            return logits
+
+model = NueralNetwork().to(device)
+print(model)
 
 
 
