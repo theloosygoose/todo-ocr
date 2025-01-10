db6f84aa22cfdf0120386b6c406a4a4c4b65bb9f
Gusti Rama Henry <g.ramahenry2439@gmail.com>
2025-01-09 19:45:19 -0500

    models and caller

--- main.py
+++ main.py
@@ -64,5 +64,57 @@ class NueralNetwork(nn.Module):
 model = NueralNetwork().to(device)
 print(model)
 
+# loss function
 
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+
+#training looop code
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (x, y) in enumerate(dataloader):
+
+        x, y = x.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(x)
+            print(f"loss: {loss:>7f} [{current:>5d}/ {size:>5d}]")
+
+
+# testing loop
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for x, y in dataloader:
+            x, y = x.to(device), y.to(device)
+            pred = model(x)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct) :>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+
+# caller
+epochs = 5
+for t in range(epochs) :
+    print(f"Epoch {t+1}\n----------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("---- Done! -----")
 
